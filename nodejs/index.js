65365885ebf257ffe7c9a9e6f2fe46906313a3f5
Laurent Grangeau <laurent.grangeau@gmail.com>
2015-11-10 21:31:42 +0100

    Add docker-compose file

--- nodejs/index.js
+++ nodejs/index.js
@@ -4,8 +4,20 @@ var async = require ('async');
 
 var app = express();
 
+var client = new cassandra.Client( { contactPoints : [ process.env.HOST ] } );
+client.connect(function(err, result) {
+    console.log('Connected.');
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/metadata', function(req, res) {
+    console.log('Host: %s', process.env.HOST);
+    res.send(client.hosts.slice(0).map(function (node) {
+        return { address : node.address, rack : node.rack, datacenter : node.datacenter }
+    }));
 });
 
 var server = app.listen(8888, function () {
