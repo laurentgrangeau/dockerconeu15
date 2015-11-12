c34d757a271fb54daf763de989de008c30fbe6c4
Laurent Grangeau <laurent.grangeau@gmail.com>
2015-11-12 14:45:21 +0100

    Change to Python

--- nodejs/index.js
+++ nodejs/index.js
@@ -12,26 +12,35 @@ app.get('/', function (req, res) {
 app.get('/mongo', function (req, res) {
   client.connect(url, function (err, db) {
     if (err) {
+      console.log(err);
       res.send(err);
     } else {
+      console.log("gros bourrin");
+
+      db.collection('hits').insertOne( { "toto": "tata" } , function (err, result) { if (err) console.log(err); console.log(result); } );
+
       var hits = db.collection('hits');
-      var hit = { ip: '', time: '' };
+      var hit = { "ip": "10.0.0.1", "time": "10" };
 
       hits.insert(hit, function (err, result) {
+        console.log("insert");
         if (err) {
+	  console.log(err);
 	  res.send(err);
 	} else {
 	  hits.count(function (err, count) {
+	    console.log("count");
 	    if (err) {
+	      console.log(err);
 	      res.send(err);
 	    } else {
 	      res.send('<h1>You are the %s visitor</h1>', count);
-	      db.close();
 	    }
 	  });
 	}
       });
     }
+    db.close();
   });
 });
 
